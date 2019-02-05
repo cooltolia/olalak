9bfd739abfa3b59490beb623cc5e386985c3e626
Anatoliy Kulak <cooltolia@gmail.com>
2019-02-06 00:01:59 +0300

    plus 2 more blocks

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,41 @@ jQuery(document).ready(function ($) {
      
 
      
+     ;
+
+     (function () {
+
      
+
+         $('.interior__slider').slick({
+
+             infinite: true,
+
+             slidesToShow: 4,
+
+             slidesToScroll: 4,
+
+             adaptiveHeight: false,
+
+             // responsive: [{
+
+             //     breakpoint: 769,
+
+             //     settings: {
+
+             //         slidesToShow: 2,
+
+             //         slidesToScroll: 1,
+
+             //     }
+
+             // }]
+
+         });
+
+     
+
+     })();
 
      
      
---
+++ develop/blocks/interior/interior.js
@@ -0,0 +1,18 @@
+;
+(function () {
+
+    $('.interior__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: false,
+        // responsive: [{
+        //     breakpoint: 769,
+        //     settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 1,
+        //     }
+        // }]
+    });
+
+})();
